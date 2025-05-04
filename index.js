a31fa275501cfabc9720d0f2f44f7ea55188947c
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-04 16:24:50 +0500

    add some code which is used in es6 modulejs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const {generateRandomNumber,celciusTofarhinheight}=require('./utils.js');
-console.log(`Random Number ${generateRandomNumber()}`)
-console.log(`Celcius to Farhinheight ${celciusTofarhinheight(0)}`);
+import {getPosts}  from './postController.js';
+// const {generateRandomNumber,celciusTofarhinheight}=require('./utils.js');
+// console.log(`Random Number ${generateRandomNumber()}`)
+// console.log(`Celcius to Farhinheight ${celciusTofarhinheight(0)}`);
+console.log(getPosts())
---
+++ postController.js
@@ -0,0 +1,13 @@
+//! Post controller
+const posts=[
+    {
+        id:1,title:'SomeOne'
+    },
+    {
+        id:2,title:'EveryOne'
+    }
+]
+const getPosts=()=>posts;
+export {
+    getPosts,
+};
