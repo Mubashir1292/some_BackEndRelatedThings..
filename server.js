9cb4fa386e3d13daa0b3f5d9d37150fbdffb90f4
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-04 18:33:52 +0500

    written some html files

--- server.js
+++ server.js
@@ -5,9 +5,21 @@ const server = http.createServer((req,res)=>{
     // res.statusCode('404');
     const url=req.url;
     const method=req.method;
+    if(url==='/'){
+        res.writeHead(200,{"content-type":"text/html"})
+        res.end("<h1>Just the Get Request...</h1>")
+    }else if(url==='/about'){
+        res.writeHead(200,{"content-type":"text.html"});
+        res.end("<h1>At About Page...</h1>");
+    }else if(url==='/contact' && method==="POST"){
+        res.end("<h1>You chooose the contact link and method is post</h1>")
+    }else{
+        res.writeHead(404,{"Content-type":"text/html"});
+        res.end("<h1>Not Founded...</h1>")
+    }
     console.log(url,method);
-    res.writeHead(200,{"content-type":"text/html"})
-    res.end("<h1>Server is on and able to write...</h1>");
+    // res.writeHead(200,{"content-type":"text/html"})
+    // res.end("<h1>Server is on and able to write...</h1>");
 });
 server.listen(port,()=>{
     console.log("server started");
