03e46c3b46d6ad7c938b164d0feee8dcfb3aae2f
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-04 17:14:25 +0500

    add the statuscode

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import http from 'http';
 const server = http.createServer((req,res)=>{
     // res.setHeader("Content-Type","text/html");
     res.setHeader("Content_Type","text/html");
-    res.statusCode('404');
+    res.statusCode='404';
     res.end("<h1>Server is on and able to write...</h1>");
 });
 server.listen(3000,()=>{
