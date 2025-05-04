3335734a7b7a976ad06d995181578403dc36a3ca
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-04 18:06:43 +0500

    .env file add

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import http from 'http';
+const port=process.env.PORT;
 const server = http.createServer((req,res)=>{
     // res.setHeader("Content_Type","text/html");
     // res.statusCode('404');
-    res.writeHead(500,{"content-type":"application/json"})
+    const url=req.url;
+    const method=req.method;
+    console.log(url,method);
+    res.writeHead(200,{"content-type":"text/html"})
     res.end("<h1>Server is on and able to write...</h1>");
 });
-server.listen(3000,()=>{
+server.listen(port,()=>{
     console.log("server started");
 })
