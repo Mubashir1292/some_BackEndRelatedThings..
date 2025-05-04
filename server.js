109e523c1c53e76297df7015e0bc7f3e4d830b6f
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-04 17:19:39 +0500

    add the writeHead

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import http from 'http';
 const server = http.createServer((req,res)=>{
-    // res.setHeader("Content-Type","text/html");
-    res.setHeader("Content_Type","text/html");
-    res.statusCode('404');
+    // res.setHeader("Content_Type","text/html");
+    // res.statusCode('404');
+    res.writeHead(500,{"content-type":"application/json"})
     res.end("<h1>Server is on and able to write...</h1>");
 });
 server.listen(3000,()=>{
