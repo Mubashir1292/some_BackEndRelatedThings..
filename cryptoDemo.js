1efed624c7258a029b0ac9e8459fa293f4aa7075
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-06 17:10:02 +0500

    added the random bytes.

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -1,10 +1,10 @@
 //! working with the crypto for Encryption and Decryption
 import crypto from 'crypto';
-const hash=crypto.createHash("sha256");
-//? this sha256 is algorithm...
-hash.update('password');
+const hash=crypto.createHash('sha256');
+hash.update("password");
 console.log(hash.digest("hex"));
+// random bytes
 crypto.randomBytes(16,(err,buf)=>{
     if(err) throw err;
-    console.log(buf.toString('hex'));
+    console.log(buf.toString("hex"))
 })
