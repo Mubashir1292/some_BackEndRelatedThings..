20e012612fdfaf0b53a1de55353b892e2f11fe58
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-06 16:10:22 +0500

    add the url

--- urlDetails.js
+++ urlDetails.js
@@ -1,7 +1,14 @@
-import url from 'url';
-const urlString='https://www.youtube.com/watch?v=32M1al-Y6Ag&t=4588s';
+import url, { URLSearchParams } from 'url';
+const urlString='https://www.youtube.com/search?q=hello+world';
 const urlObj=new URL(urlString);
 console.log(urlObj);
+// Anyone can find out the properties of any url by passing it to the URL function..
 // formate 
 // ? this format will take the object and returns back the string
 console.log(url.format(urlObj));
+console.log(url.fileURLToPath(import.meta.url));
+// ! getting the query params
+const params=new URLSearchParams(urlObj.search);
+params.append("limit","5");
+params.delete("limit");
+console.log(params);
