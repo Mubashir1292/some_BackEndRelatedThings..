7ceacc69c1350f093862c7f7760482841eb64417
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-06 13:36:51 +0500

    read file using callbacks and promises as well

--- fsDemo.js
+++ fsDemo.js
@@ -1,7 +1,23 @@
-import fs from 'fs';
-fs.readFile('./test.txt','utf-8',(error,data)=>{
-    if(error) throw error;
-    console.log(data);
-});
-const data=fs.readFileSync('./test.txt','utf-8')
-console.log(data);
+import { webcrypto } from 'crypto'
+import fs from 'fs/promises'
+// import fs from 'fs';
+// fs.readFile('./test.txt','utf-8',(error,data)=>{
+//     if(error) throw error;
+//     console.log(data);
+// });
+// const data=fs.readFileSync('./test.txt','utf-8')
+// console.log(data);
+// fs.readfileAsync('./test.txt','utf-8')
+// .then((data)=>console.log(data))
+// .catch(error=>console.log(error));
+
+fs.readFile('./test.txt','utf-8').then(data=>console.log(data)).catch(error=>console.error(error));
+const readfile=async()=>{
+    try{
+        const data = await fs.readFile('./test.txt','utf-8');
+        console.log(data);
+    }catch(error){
+        console.log(error)
+    }
+}
+readfile();
