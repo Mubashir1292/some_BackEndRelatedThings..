47ac7b5acc5cec68153891f8d615f0dce51b399a
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-06 18:31:21 +0500

    add the crypto for encryption and decryption

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -1,10 +1,23 @@
 //! working with the crypto for Encryption and Decryption
-import crypto from 'crypto';
+import crypto, { randomBytes } from 'crypto';
 const hash=crypto.createHash('sha256');
 hash.update("password");
-console.log(hash.digest("hex"));
+// console.log(hash.digest("hex"));
 // random bytes
 crypto.randomBytes(16,(err,buf)=>{
     if(err) throw err;
-    console.log(buf.toString("hex"))
+    // console.log(buf.toString("hex"))
 })
+// ? createCipheriv & createDecipheriv
+const algorithm="aes-256-cbc";
+const key=crypto.randomBytes(32);
+const iv=crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm,key,iv);
+let encrypted = cipher.update("hy","utf-8","hex");
+encrypted+=cipher.final("hex");
+console.log(encrypted);
+const decipher = crypto.createDecipheriv(algorithm,key,iv);
+let dataDecrypted = decipher.update(encrypted,"hex","utf-8");
+dataDecrypted+=decipher.final("utf-8");
+console.log(dataDecrypted);
